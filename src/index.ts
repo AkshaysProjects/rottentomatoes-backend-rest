b1f31191b5031e8672ffb59d60e892810b29b527
KakatkarAkshay <akshayakakatkaraa@gmail.com>
2024-06-06 00:29:01 +0000

    feat: :sparkles: Initialize an express app

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello, world!");
+import express, { Response } from "express";
+
+// Create a new express application instance
+const app = express();
+
+// Define a route handler for the default home page
+app.get("/", (_req, res: Response) => {
+  res.send("Hello World");
+});
+
+// Function to start the app
+function startApp() {
+  // TODO: Use a port from environment variables
+  app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+  });
+}
+
+// Start the app
+startApp();
