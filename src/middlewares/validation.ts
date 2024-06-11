ae5d18650da3a5f6a07a59b4b375028396f92ad1
KakatkarAkshay <akshayakakatkaraa@gmail.com>
2024-06-11 09:55:25 +0000

    docs: :bulb: Add comments to validation middleware

--- src/middlewares/validation.ts
+++ src/middlewares/validation.ts
@@ -2,17 +2,26 @@ import { NextFunction, Request, Response } from "express";
 import { z, ZodError } from "zod";
 
 export function validateData(schema: z.ZodObject<any, any>) {
+  // Return a middleware function
   return (req: Request, res: Response, next: NextFunction) => {
     try {
+      // Parse the request body
       req.body = schema.parse(req.body);
-      next();
+
+      // Call the next middleware
+      return next();
     } catch (error) {
+      // Check if the error is a ZodError
       if (error instanceof ZodError) {
+        // Map the errors to a more readable format
         const errorMessages = error.errors.map((issue: any) => ({
           message: `${issue.path.join(".")} is ${issue.message}`,
         }));
+
+        // Return the error response
         res.status(400).json({ error: "Invalid data", details: errorMessages });
       } else {
+        // Return a generic error response
         res.status(500).json({ error: "Internal Server Error" });
       }
     }
